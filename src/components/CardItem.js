47945d2c31e2dd865b66d82510cfca3996bfcda7
Matheus Eduardo <playsmts@hotmail.com>
2022-03-14 12:12:31 -0300

    updated cards

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -2,13 +2,15 @@ import React from 'react';
 // import { Link } from 'react-router-dom';
 
 function CardItem(props) {
+    let lang = props.lang.toLowerCase();
+    let classNameByObject = `cards__item__pic-wrap_${lang}`;
   return (
       <>
         <li className='cards__item' style={{cursor: "pointer"}}> 
             {/* a -> link */}
             {/* onClick={() => openWebsite(props.url)} */}
             <a className='cards__item__link' href={props.url} target="_blank" rel="noreferrer">
-                <figure className='cards__item__pic-wrap' data-category={props.label}>
+                <figure className={classNameByObject} data-category={props.label}>
                     <img src={props.src} alt="card_image" className='cards__item__img' />   
                 </figure>
                 <div className='cards__item__info'>
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,7 +5,8 @@ import image01 from './images/twitter-to-discord.jpg'
 import image02 from './images/insta-bio-updater.jpg'
 import image03 from './images/calor-bot.jpg'
 import image04 from './images/lightshot-scrapper.jpg'
-import image05 from './images/insta-sorteio.jpg'
+// import image05 from './images/insta-sorteio.jpg'
+import image06 from './images/business-website.jpg'
 
 function Cards() {
   return (
@@ -19,11 +20,13 @@ function Cards() {
                     src={image01} 
                     text="Twitter Media to Discord" 
                     label='Python' 
+                    lang='Python'
                     url='https://github.com/euMts/twitter_media_to_discord' />
                     <CardItem 
                     src={image02} 
                     text="Instagram Bio Updater" 
                     label='Python' 
+                    lang='Python'
                     url='https://github.com/euMts/instagram_bio_updater' />
                 </ul>    
                 <ul className='cards__items'>
@@ -31,17 +34,20 @@ function Cards() {
                     src={image03} 
                     text="Bot do Calor" 
                     label='Python' 
+                    lang='Python'
                     url='https://github.com/euMts/bot_calor' />
                     <CardItem 
                     src={image04} 
                     text="Lightshot Scrapper" 
                     label='Python' 
+                    lang='Python'
                     url='https://github.com/euMts/Lightshot-Scraper' />
                     <CardItem 
-                    src={image05} 
-                    text="Instagram Auto Commenter" 
-                    label='Python' 
-                    url='https://github.com/euMts/instaSorteio' />
+                    src={image06} 
+                    text="Business Website" 
+                    label='React' 
+                    lang='React'
+                    url='https://github.com/euMts/euMts.github.io' />
                 </ul>    
             </div>  
         </div>
